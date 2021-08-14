37b68f2d9a00c6c3b938c93c51160554a93f95d1
Luciano Gazai <luciano.ramo76@gmail.com>
2021-08-14 09:52:47 -0300

    select ajustado

--- index.js
+++ index.js
@@ -17,11 +17,54 @@ async function fetchApi(sun, water, pets) { //handle status 404
     }
 }
 
-const writeData = (plants) => {
+const writeData2 = (plants) => {
     const container = document.getElementById('div-results')
     container.innerHTML = '<div class="grid grid-template-rows-4">' + plants.map((plant) => {
-        return '<div class="item"> <div class="imgGrid"><img src="'+plant.url + '"></img></div>' + '<h4>'+plant.name + '</h4></div>'
+        return '<div class="item"> <div class="imgGrid"><img src="' + plant.url + '"></img></div>' + '<h4>' + plant.name + '</h4></div>'
     }).join('') + '</div>'
+}
+
+const writeData = (plants) => {
+    const container = document.getElementById('div-results')
+    container.innerHTML=""
+    plants.map((plant, id) => {
+        var template = document.createElement('template');
+        if (id === 0) {
+            template.innerHTML = `
+            <div class="item item-${id+1}">
+                <div class="imgGrid"><img src="${plant.url}"></div>
+                <h4>${plant.name}</h4>
+                <div class="planta-footer">
+                    <div class="planta-preco">
+                        <h4>$${plant.price}</h4>
+                    </div>
+                    <div class="planta-icones">
+                        <div class="planta-icone sun"><img src="./images/icons/low-sun.svg" alt=""></div>
+                        <div class="planta-icone water"><img src="./images/icons/3-drops.svg" alt=""></div>
+                        <div class="planta-icone pets"><img src="./images/icons/pet.svg" alt=""></div>
+                    </div>
+                </div>
+                </div>`.trim()
+            container.appendChild(template.content.firstChild)
+        } else {
+            template.innerHTML = `<div class="item item-${id+1}">
+                <div class="imgGrid"><img src="${plant.url}"></div>
+                <h4>${plant.name}</h4>
+                <div class="planta-footer">
+                    <div class="planta-preco">
+                        <h4>$${plant.price}</h4>
+                    </div>
+                    <div class="planta-icones">
+                        <div class="planta-icone sun"><img src="./images/icons/low-sun.svg" alt=""></div>
+                        <div class="planta-icone water"><img src="./images/icons/3-drops.svg" alt=""></div>
+                        <div class="planta-icone pets"><img src="./images/icons/pet.svg" alt=""></div>
+                    </div>
+                </div>
+                </div>`.trim()
+            container.appendChild(template.content.firstChild)
+        }
+    })
+
 }
 
 const cleanResults = () => {
@@ -75,22 +118,32 @@ const handlePetsSelect = (el) => {
 const petsSelect = document.getElementById('petsSelector');
 petsSelect.addEventListener('change', handlePetsSelect, false)
 
-document.querySelector('.custom-select-wrapper').addEventListener('click', function() {
+document.querySelector('.custom-select-wrapper').addEventListener('click', function () {
     this.querySelector('.custom-select').classList.toggle('open');
+    this.querySelector('.custom-select__trigger').classList.toggle('trigger_open')
+    console.log('foi')
 })
 
 for (const option of document.querySelectorAll(".custom-option")) {
-    option.addEventListener('click', function() {
+    option.addEventListener('click', function () {
         if (!this.classList.contains('selected')) {
             this.parentNode.querySelector('.custom-option.selected').classList.remove('selected');
             this.classList.add('selected');
             this.closest('.custom-select').querySelector('.custom-select__trigger span').textContent = this.textContent;
+            //this.parentNode.parentNode.firstChild.nextSibling.classList.toggle('trigger_open')
+
         }
+        
+        console.log(this)
+        
     })
 }
-window.addEventListener('click', function(e) {
+window.addEventListener('click', function (e) {
     const select = document.querySelector('.custom-select')
+    const trigger = document.querySelector('.custom-select__trigger')
     if (!select.contains(e.target)) {
         select.classList.remove('open');
+        trigger.classList.remove('trigger_open')
     }
+    
 });
